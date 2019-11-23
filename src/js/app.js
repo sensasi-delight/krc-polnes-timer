2a4d0da0c4bb7ee8c0881b9fa905f254f1624588
egin10 <egin066@gmail.com>
2019-11-23 09:45:39 +0700

    #e** merapihkan tampilan & menambah page settings

--- src/js/app.js
+++ src/js/app.js
@@ -7,39 +7,36 @@ const handleChange = e => {
 }
 
 const routeToPage = (page) => {
-    console.log(page);
     switch (page){
-        case 'listTeam':
-            pageListTeam();
-            break;
         case 'stopWatch':
-            pageStopWatch();
+            showPage('stopwatch', 'bgImage1');
+            break;
+        case 'listTeam':
+            showPage('list-team', 'bgImage2');
+            break;
+        case 'settings':
+            showPage('setting', 'bgImage3');
             break;
     }
 }
 
-const pageListTeam = () => {
+const showPage = (page, bgImage) => {
+    //page
     const ListTeamPage = document.getElementById('ListTeamPage');
     const StopWatchPage = document.getElementById('StopWatchPage');
+    const Settings = document.getElementById('Settings');
+    
+    ListTeamPage.style.display  = (page == 'list-team') ? 'block' : 'none';
+    StopWatchPage.style.display = (page == 'stopwatch') ? 'block' : 'none';
+    Settings.style.display      = (page == 'setting') ? 'block' : 'none';
+
+    //background
     const bgImage1 = document.getElementById('bg-image1');
     const bgImage2 = document.getElementById('bg-image2');
+    const bgImage3 = document.getElementById('bg-image3');
     
-    bgImage1.style.display = 'none'
-    StopWatchPage.style.display = 'none';
+    bgImage1.style.display = (bgImage == 'bgImage1') ? 'block' : 'none';
+    bgImage2.style.display = (bgImage == 'bgImage2') ? 'block' : 'none';
+    bgImage3.style.display = (bgImage == 'bgImage3') ? 'block' : 'none';
     
-    bgImage2.style.display = 'block'
-    ListTeamPage.style.display = 'block';
-}
-
-const pageStopWatch = () => {
-    const ListTeamPage = document.getElementById('ListTeamPage');
-    const StopWatchPage = document.getElementById('StopWatchPage');
-    const bgImage1 = document.getElementById('bg-image1');
-    const bgImage2 = document.getElementById('bg-image2');
-
-    bgImage1.style.display = 'block'
-    StopWatchPage.style.display = 'block';
-    
-    bgImage2.style.display = 'none'
-    ListTeamPage.style.display = 'none';
 }
